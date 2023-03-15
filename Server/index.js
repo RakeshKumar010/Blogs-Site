5f4394e8e1cdeaf793d1cc5a30872332a2c02646
Rakesh Kumar <rakeshkumar748844@gmail.com>
2023-03-15 22:25:47 +0530

    clientapi

---
+++ Server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config({path:'./config.env'})
+const PORT = process.env.PORT;
+require('./db/conn')
+app.use(express.json())
+app.use(require('./router/router'))
+
+app.listen(PORT,()=>{
+    console.log('Server is onpen at localhost'+PORT)
+})
---
+++ Server/router/router.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const Router = express.Router()
+const userSchema = require('../schema/userSchema')
+const productSchema = require('../schema/productSchema')
+Router.get('/', async (req, res) => {
+    let result = await productSchema.find()
+    res.send(result)
+
+})
+
+
+
+//signup && login
+Router.post('/admin/signup', async (req, res) => {
+    const { name, email, password, cpassword } = req.body;
+    if (name && email && password && cpassword) {
+        let findData = await userSchema.findOne({ email })
+        if (findData) {
+            res.send({ 'error': "email is already present" })
+        } else {
+            if (password == cpassword) {
+                let result = new userSchema(req.body)
+                result = await result.save()
+                res.send(result)
+            } else {
+                res.send({ "Error": "Both password is must be same" })
+            }
+
+        }
+
+    } else {
+        res.json({ "error": "fill all details" })
+    }
+})
+
+Router.post('/admin/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+        let result = await userSchema.findOne(req.body)
+        if (result) {
+            res.send(result)
+
+        } else {
+            res.send({ "Error": "Data not found" })
+        }
+    } else {
+        res.send({ "error": "fill Both field" })
+    }
+})
+
+
+//product
+Router.post('/admin/addProduct', async (req, res) => {
+    let { topic, header, content } = req.body;
+    if (topic && header && content) {
+        let findData = await productSchema.findOne({ header })
+        if (findData) {
+            res.send({ 'error': "this headeing is available" })
+
+
+        } else {
+            let result = new productSchema(req.body)
+            result = await result.save()
+            res.send(result)
+        }
+
+    } else {
+        res.send({ 'error': 'fill all field' })
+    }
+
+})
+Router.delete('/admin/delete/:id', async (req, res) => {
+    let result = await productSchema.deleteOne({ _id: req.params.id })
+    res.send(result)
+})
+Router.put('/admin/updateProduct/:id', async (req, res) => {
+    let result = await productSchema.updateOne({ _id: req.params.id }, { $set: req.body })
+    res.send(result)
+})
+Router.get('/admin', async (req, res) => {
+    let result = await productSchema.find()
+    res.send(result)
+
+})
+module.exports = Router;
